0873b2bd7d48ee1884831a7bc1a289998bdada4f
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-10 22:57:01 +0300

    Performed recommendations

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,8 +5,8 @@ from cinema.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True)
-    description = serializers.CharField(required=True)
+    title = serializers.CharField()
+    description = serializers.CharField()
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
